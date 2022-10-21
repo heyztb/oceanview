096e6951ab7de417c15f9227cff54dc5bda32590
Zach Blake <ztb@pm.me>
2022-10-20 17:25:37 -0700

    recommended to require daisy AFTER typography

--- app/tailwind.config.js
+++ app/tailwind.config.js
@@ -6,8 +6,8 @@ module.exports = {
     extend: {},
   },
   plugins: [
+    require("@tailwindcss/typography"),
     require('daisyui'),
-    require("@tailwindcss/typography")
   ],
   daisyui: {
     styled: true,
